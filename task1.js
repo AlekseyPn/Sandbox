b5b29c24871301efc6eb4cdae54147db2dd91dc0
Peresmekhin Aleksey <aleksfers@icloud.com>
2017-10-07 17:22:45 +0500

    Клонирование объектов

--- task1.js
+++ task1.js
@@ -1,7 +1,7 @@
 (function () {
     var OPERATION_STRING = 'Введите математическую операцию которую нужно выполнить \'div\', \'sum\', \'mult\''
     var ENTER_NUMBER = 'Введите число.'
-    var WARNING_MSG = 'Делить на ноль нельзя! Введите другое число или операцию.'    
+    var WARNING_MSG = 'Делить на ноль нельзя! Введите другое число или операцию.'
     var math = function (operation) {
         var num1 = query(ENTER_NUMBER)
         var num2 = query(ENTER_NUMBER)
@@ -18,12 +18,26 @@
                 } else {
                     alert(WARNING_MSG)
                     math(query(OPERATION_STRING))
-                }        
+                }
                 break
-        }        
+        }
     }
     var query = function (string) {
         return prompt(string, '')
     }
-    math(query(OPERATION_STRING))
+
+    var cloneObject = {
+        simple: function (obj) {
+            return obj
+        },
+        full: function (obj) {
+            // Два способа клонирования, старый добрый for..in и ES2015 ^_^
+            // var clone = {}
+            // for (key in obj) {
+            //     clone[key] = obj[key]
+            // }
+            // return clone
+            return Object.assign({}, obj)
+        }
+    }    
 })()
