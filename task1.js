fdc80eb5c850cbfa775e096336fb7b0957c493d8
Peresmekhin Aleksey <aleksfers@icloud.com>
2017-10-11 20:30:46 +0500

    Fix mistakes, and refactor on pure JS

--- task1.js
+++ task1.js
@@ -1,30 +1,24 @@
 (function () {
-    var OPERATION_STRING = 'Введите математическую операцию которую нужно выполнить \'div\', \'sum\', \'mult\''
-    var ENTER_NUMBER = 'Введите число.'
     var WARNING_MSG = 'Делить на ноль нельзя! Введите другое число или операцию.'
-    var math = function (operation) {
-        var num1 = query(ENTER_NUMBER)
-        var num2 = query(ENTER_NUMBER)
+    var math = function (operation, num1, num2) {
+        
         switch (operation) {
             case "sum":
-                alert(parseInt(num1 + num2))
+                consoel.log(parseInt(num1 + num2))
                 break
             case "mult":
-                alert(num1 * num2)
+                consoel.log(num1 * num2)
                 break
             case "div":
                 if (num2 !== '0') {
-                    alert(num1 / num2)
+                    consoel.log(num1 / num2)
                 } else {
-                    alert(WARNING_MSG)
-                    math(query(OPERATION_STRING))
+                    consoel.log(WARNING_MSG)                    
                 }
                 break
         }
     }
-    var query = function (string) {
-        return prompt(string, '')
-    }
+
 
     var cloneObject = {
         simple: function (obj) {
